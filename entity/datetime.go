7de82974908c8bdaccfdcc7706cd716c65ccbf65
ah.jo <ah.jo@navercorp.com>
2023-02-19 15:43:04 +0900

    add: reporting

---
+++ entity/datetime.go
@@ -0,0 +1,77 @@
+package entity
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+type DateTime struct {
+	time.Time
+}
+
+func (t DateTime) FirstDayOfMonth() DateTime {
+	year, month, _ := t.Date()
+	currentLocation := t.Location()
+
+	return DateTime{time.Date(year, month, 1, 0, 0, 0, 0, currentLocation)}
+}
+
+func (t DateTime) LastDayOfMonth() DateTime {
+	return DateTime{Time: t.FirstDayOfMonth().AddDate(0, 1, 0).Time.Add(-time.Nanosecond)}
+}
+
+func (t DateTime) Between(before DateTime, after DateTime) bool {
+	return (t.After(before) || t.Equal(before)) && (t.Before(after) || t.Equal(after))
+}
+
+func (t DateTime) After(u DateTime) bool {
+	return t.Time.After(u.Time)
+}
+
+func (t DateTime) Equal(u DateTime) bool {
+	return t.Time.Equal(u.Time)
+}
+
+func (t DateTime) Before(u DateTime) bool {
+	return t.Time.Before(u.Time)
+}
+
+func (t DateTime) Add(d time.Duration) DateTime {
+	return DateTime{t.Time.Add(d)}
+}
+
+func (t DateTime) Sub(u DateTime) time.Duration {
+	return t.Time.Sub(u.Time)
+}
+
+func (t DateTime) AddDate(years int, months int, days int) DateTime {
+	return DateTime{t.Time.AddDate(years, months, days)}
+}
+
+func (t DateTime) Min(u DateTime) DateTime {
+	if t.Before(u) {
+		return t
+	} else {
+		return u
+	}
+}
+
+// Scan https://gorm.io/docs/data_types.html#Implements-Customized-Data-Type
+func (t *DateTime) Scan(b interface{}) (err error) {
+	switch x := b.(type) {
+	case time.Time:
+		t.Time = x
+	default:
+		err = fmt.Errorf("unsupported scan type %T", b)
+	}
+	return
+}
+
+// Value https://gorm.io/docs/data_types.html#Implements-Customized-Data-Type
+func (t DateTime) Value() (driver.Value, error) {
+	if t.Time.IsZero() {
+		return nil, nil
+	}
+	return t.Time.Format("2006-01-02"), nil
+}
--- entity/entity.go
+++ entity/entity.go
@@ -1,7 +1,5 @@
 package entity
 
-import "time"
-
 type TimerType int
 
 const (
@@ -11,38 +9,40 @@ const (
 )
 
 type ToDo struct {
-	Id            uint      `json:"id"`
-	UserId        uint      `json:"userId"`
-	Content       string    `gorm:"not null" json:"content"`
-	Completed     bool      `json:"completed"`
-	Private       bool      `json:"private"`
-	CreatedTime   time.Time `json:"createdTime"`
-	CompletedTime time.Time `json:"completedTime"`
-	ModifiedTime  time.Time `json:"modifiedTime"`
-	DeletedTime   time.Time `json:"deletedTime"`
+	Id            uint     `json:"id"`
+	UserId        uint     `gorm:"not null" json:"userId"`
+	Content       string   `gorm:"not null" json:"content"`
+	Completed     bool     `json:"completed"`
+	CreatedTime   DateTime `json:"createdTime"`
+	CompletedTime DateTime `json:"completedTime"`
+	ModifiedTime  DateTime `json:"modifiedTime"`
+	DeletedTime   DateTime `json:"deletedTime"`
 }
 
 type Timer struct {
 	Id        uint      `json:"id"`
-	Name      string    `json:"name"`
-	MakerId   uint      `json:"makerId"`
+	Name      string    `gorm:"not null" json:"name"`
+	MakerId   uint      `gorm:"not null" json:"makerId"`
 	Type      TimerType `gorm:"not null" json:"type"`
 	Tags      string    `json:"tags"`
 	LinkUrl   *string   `json:"linkUrl"`
-	StartTime time.Time `json:"startTime"`
-	EndTime   time.Time `json:"endTime"`
+	Users     []*User   `gorm:"many2many:participants;"`
+	StartTime DateTime  `gorm:"not null" json:"startTime"`
+	EndTime   *DateTime `json:"endTime"`
+}
+
+type TimeRecord struct {
+	Id        uint     `json:"id"`
+	UserId    uint     `json:"userId"`
+	TimerId   uint     `json:"timerId"`
+	StartTime DateTime `json:"startTime"`
+	EndTime   DateTime `json:"endTime"`
 }
 
 type User struct {
-	Id       uint   `json:"id"`
-	UserName string `json:"username"`
-	Password string `json:"password"`
-}
-
-type Participant struct {
-	Id        uint      `json:"id"`
-	UserId    uint      `json:"userId"`
-	TableId   uint      `json:"tableId"`
-	StartTime time.Time `json:"startTime"`
-	EndTime   time.Time `json:"endTime"`
+	Id         uint    `json:"id"`
+	UserName   string  `json:"username"`
+	Password   string  `json:"password"`
+	Onboarding bool    `json:"onboarding"`
+	Timers     []Timer `gorm:"many2many:participants;"`
 }
