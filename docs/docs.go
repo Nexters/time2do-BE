a648b578acdbf3d5c8c42049e36921c281032dfd
Gunho Park <devgun@kakao.com>
2023-02-25 19:46:06 +0900

    fix: docs: swagger

--- docs/docs.go
+++ docs/docs.go
@@ -134,7 +134,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "time2do.me:8888",
+	Host:             "0.0.0.0:8888",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Orders API",
