17405a1afd93874c4eda12921bd9d163f9245116
Gunho Park <devgun@kakao.com>
2023-02-27 22:40:21 +0900

    fix: data.sql

--- scripts/data.sql
+++ scripts/data.sql
@@ -11,13 +11,13 @@ INSERT INTO T2D_DB.timers
 VALUES (2, '그룹 타이머', 1, 2, '태그1, 태그2, 태그3', null, '2023-02-19', '2023-02-20');
 
 INSERT INTO T2D_DB.to_dos
-VALUES (1, 1, '밥먹기', false, false, '2023-02-01', null, null, null);
+VALUES (1, 1, '밥먹기', '0', '2023-02-01', null);
 
 INSERT INTO T2D_DB.to_dos
-VALUES (2, 1, '잠자기', false, false, '2023-02-01', null, null, null);
+VALUES (2, 1, '밥먹기', '0', '2023-02-01', null);
 
 INSERT INTO T2D_DB.to_dos
-VALUES (3, 1, '숨쉬기', false, false, '2023-02-01', null, null, null);
+VALUES (3, 1, '밥먹기', '0', '2023-02-01', null);
 
 INSERT INTO T2D_DB.time_records
 VALUES (1, 1, 1, '2023-02-01 00:00:00', '2023-02-01 06:00:00');
